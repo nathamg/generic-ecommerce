974fe08d0558bb99fa50687caf7114ea52d6c496
Nathaly Mesa <nathaly.mesa@globant.com>
2021-08-06 14:26:05 -0500

    build: Add populate npm script docs: Update readme

--- README.md
+++ README.md
@@ -4,6 +4,23 @@ Basic project containing a REST API implemented in Node.js for an E-Commerce off
 
 ## Requirements
 
-## Run
+- Node.js
+- PostgresDB
 
-## Test
+## Run locally
+
+Please use `.env.example` as a guide to create a `.env` file and fill it with the details of your database connection.
+
+```
+npm install
+npm run build
+npm start
+```
+
+The project includes migrations and seeds using `knex` so you can populate your database and start testing the service right away.
+
+```
+npm run migrate
+npm run populate:db
+
+```
--- src/config/seeds/dev.ts
+++ src/config/seeds/dev.ts
@@ -1,14 +1,17 @@
 import { Knex } from 'knex';
 
 export async function seed(knex: Knex): Promise<void> {
+  await knex('baskets_items').del();
+  await knex('baskets').del();
+  await knex('discounts').del();
   await knex('items').del();
+
   await knex('items').insert([
     { id: 1, code: 'PEN', name: 'Pen', price: 5 },
     { id: 2, code: 'TSHIRT', name: 'T-shirt', price: 20 },
     { id: 3, code: 'MUG', name: 'Coffee Mug', price: 7.5 },
   ]);
 
-  await knex('discounts').del();
   await knex('discounts').insert([
     {
       id: 1,
@@ -24,5 +27,25 @@ export async function seed(knex: Knex): Promise<void> {
       type: 'FROM_MIN_QUANTITY',
       itemId: 2,
     },
+  ]);
+
+  await knex('baskets').insert([
+    { id: 1, creationDate: '2021-08-06', totalPrice: 0 },
+    { id: 2, creationDate: '2021-08-06', totalPrice: 0 },
+    { id: 3, creationDate: '2021-08-06', totalPrice: 0 },
+    { id: 4, creationDate: '2021-08-06', totalPrice: 0 },
+  ]);
+
+  await knex('baskets_items').insert([
+    { id: '1', itemId: '1', basketId: '1', itemsQuantity: 1 },
+    { id: '2', itemId: '2', basketId: '1', itemsQuantity: 1 },
+    { id: '3', itemId: '3', basketId: '1', itemsQuantity: 1 },
+    { id: '4', itemId: '1', basketId: '2', itemsQuantity: 2 },
+    { id: '5', itemId: '2', basketId: '2', itemsQuantity: 1 },
+    { id: '6', itemId: '1', basketId: '3', itemsQuantity: 1 },
+    { id: '7', itemId: '2', basketId: '3', itemsQuantity: 4 },
+    { id: '8', itemId: '1', basketId: '4', itemsQuantity: 3 },
+    { id: '9', itemId: '2', basketId: '4', itemsQuantity: 3 },
+    { id: '10', itemId: '3', basketId: '4', itemsQuantity: 1 },
   ]);
 }
